309c2341db698e7def88635f0692cbd3ab9fed36
Qian Yun Zhang <zhangqy@umich.edu>
2026-02-27 10:33:12 -0500

    working timer publisher

--- my_test_publisher.py
+++ my_test_publisher.py
@@ -5,24 +5,24 @@ from std_msgs.msg import Int32MultiArray
 
 TOPIC = "/gui/slider_moved"
 
-class Pub(Node):
+class SliderEventPublisher(Node):
     def __init__(self):
         super().__init__("slider_event_publisher")
-        self.pub = self.create_publisher(Int32MultiArray, TOPIC, 10)
-        self.i = 0
-        self.timer = self.create_timer(1.0, self.tick)
+        self.publisher = self.create_publisher(Int32MultiArray, TOPIC, 10)
+        self.value = 0
+        self.timer = self.create_timer(1.0, self.timer_callback)
         self.get_logger().info(f"Publishing on {TOPIC}")
 
-    def tick(self):
+    def timer_callback(self):
         msg = Int32MultiArray()
-        msg.data = [0, self.i]
-        self.pub.publish(msg)
-        self.get_logger().info(f"Published {msg.data}")
-        self.i = (self.i + 1) % 101
+        msg.data = [0, self.value]
+        self.publisher.publish(msg)
+        self.get_logger().info(f"Published: {msg.data}")
+        self.value = (self.value + 1) % 101
 
 def main():
     rclpy.init()
-    node = Pub()
+    node = SliderEventPublisher()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
